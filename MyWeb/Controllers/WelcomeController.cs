e9dde29726358c35c2c9fd51b2666d5f3fc88867
91 <jay.hatelove@gmail.com>
2014-11-20 11:27:15 +0800

    add Login Controller and View, add WelcomeController and View

---
+++ MyWeb/Controllers/WelcomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWeb.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        //
+        // GET: /Welcome/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
